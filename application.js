5cd6b2c9902b76ebd1424d71be1c8184ae3c2300
Aaron Moller <moller.aaron@gmail.com>
2016-10-17 10:15:32 -0400

    add slides

--- application.js
+++ application.js
@@ -1,6 +1,26 @@
 App.onLaunch = function(options) {
+
+    const baseURL = options.baseURL;
+
+
     var template = '<document><loadingTemplate><activityIndicator><text>Hello World!</text></activityIndicator></loadingTemplate></document>';
+
+
+
+
+
+
     var templateParser = new DOMParser();
     var parsedTemplate = templateParser.parseFromString(template, "application/xml");
     navigationDocument.pushDocument(parsedTemplate);
+
+
+
+
+
+    // Show a loading spinner while additional JavaScript files are being evaluated
+    const loadingDocument = createLoadingDocument();
+    navigationDocument.pushDocument(loadingDocument);
+
+
 }
